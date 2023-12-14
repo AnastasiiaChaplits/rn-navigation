5ed0589c6408efaf3048670f856dfd408e5a84da
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2023-12-14 13:58:58 +0200

    style meal details screen

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,69 @@
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
+
+const MealDetailScreen = ({ route }) => {
+  const { mealId } = route.params;
+
+  const {
+    imageUrl,
+    title,
+    duration,
+    complexity,
+    affordability,
+    ingredients,
+    steps,
+  } = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image source={{ uri: imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{title}</Text>
+      <MealDetails
+        duration={duration}
+        complexity={complexity}
+        affordability={affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    maxWidth: "80%",
+  },
+});
+
+export default MealDetailScreen;
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -19,6 +19,7 @@ const MealsOverviewScreen = ({ route, navigation }) => {
 
   const renderMealItem = ({ item }) => {
     const mealItemProps = {
+      id: item.id,
       title: item.title,
       imageUrl: item.imageUrl,
       affordability: item.affordability,
