769139e59d67219c4547005aca2592ad6891baed
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2024-01-11 11:00:40 +0200

    add favourite meals screen

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,7 +1,34 @@
-import { Text } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import {useContext} from "react";
+import {StyleSheet, View, Text} from 'react-native';
+import {FavoritesContext} from "../store/context/favorites-context";
+import {MEALS} from "../data/dummy-data";
 
 const FavouritesScreen = () => {
-  return <Text>Hello</Text>;
+  const favouriteMealsCtx = useContext(FavoritesContext);
+
+  const favouriteMeals = MEALS.filter(meal => favouriteMealsCtx.ids.includes(meal.id))
+
+  if (favouriteMeals.length === 0) {
+    return <View style={styles.rootContainer}>
+      <Text style={styles.text}>You have no favourite meals yet.</Text>
+    </View>
+  }
+
+  return <MealsList items={favouriteMeals} />
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+})
 
 export default FavouritesScreen;
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,8 +1,6 @@
-import { View, StyleSheet, FlatList } from "react-native";
-
 import { CATEGORIES, MEALS } from "../data/dummy-data";
-import MealItem from "../components/MealItem";
 import { useLayoutEffect } from "react";
+import MealsList from "../components/MealsList/MealsList";
 
 const MealsOverviewScreen = ({ route, navigation }) => {
   const catId = route.params.categoryId;
@@ -17,34 +15,9 @@ const MealsOverviewScreen = ({ route, navigation }) => {
     navigation.setOptions({ title });
   }, [catId, navigation]);
 
-  const renderMealItem = ({ item }) => {
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imageUrl: item.imageUrl,
-      affordability: item.affordability,
-      complexity: item.complexity,
-      duration: item.duration,
-    };
-    return <MealItem {...mealItemProps} />;
-  };
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealsList items={displayedMeals}/>
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
 
 export default MealsOverviewScreen;
